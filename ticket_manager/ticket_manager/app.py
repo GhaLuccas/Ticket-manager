fbd57cb7edb1bda60ab64e7f3c2a8ffc84354b52
ghaluccas <gabsgit@gmail.com>
2025-02-17 10:49:57 -0300

    montando paginas

--- ticket_manager/ticket_manager/app.py
+++ ticket_manager/ticket_manager/app.py
@@ -2,8 +2,17 @@
 from fastapi import FastAPI
 
 from ticket_manager.routers import auth, clients, tickets, users
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
 app.include_router(users.users_router)
 app.include_router(clients.clients_router)
 app.include_router(auth.auth_router)
